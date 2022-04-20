7db550e5187aa598cf1f0b404767e3cbab1d541f
Distrikt64 <8573006+JulienVanelian@users.noreply.github.com>
2022-04-21 00:09:04 +0200

    Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# nuxt3-reproduction-node-fetch
+
+`yarn install`
+`yarn deploy`
---
+++ app.vue
@@ -0,0 +1,7 @@
+<template></template>
+
+<script setup>
+
+fetch("https://api.github.com/users/julienvanelian/repos") // Commenting this line will make the error disappear
+
+</script>
